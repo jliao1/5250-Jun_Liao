12098b61c04fcc2db5035d03d17697b725ce2f37
june.liao <june.liao.bashpole@gmail.com>
2022-01-23 13:37:42 -0800

    Added Read Method to ItemIndexViewModel

--- Mine/Mine/ViewModels/ItemIndexVeiwModel.cs
+++ Mine/Mine/ViewModels/ItemIndexVeiwModel.cs
@@ -10,6 +10,10 @@ using Mine.Views;
 
 namespace Mine.ViewModels
 {
+    /// <summary>
+    /// All of our data operations will start in the <type>IndexViewModel 
+    /// (consider this the entry point to the datastore)
+    /// </summary>
     public class ItemIndexVeiwModel : BaseViewModel
     {
         public ObservableCollection<ItemModel> Items { get; set; }
@@ -54,5 +58,27 @@ namespace Mine.ViewModels
                 IsBusy = false;
             }
         }
+
+        /// <summary>
+        /// Call to the DataStore.ReadAsync passing the id, get an
+        /// item record from the datastore into a variable called result
+        /// and then return result. This is to make debugging, maintenance easier.
+        /// 
+        /// The reason we need this method is defensive programing. We 
+        /// want to hookup Delete, before delete an item, we want to 
+        /// check if it exists first, so we need defensive programing.
+        /// There is a read method in the MockDataStore, so just need to 
+        /// call to it. This “ReadAsync” method that will read the item 
+        /// from the datastore, If the item exist, then Delete can continue.
+
+        /// </summary>
+        /// <param name="id">Id of the Record</param>
+        /// <returns>The record from ReadAsync</returns>
+        public async Task<ItemModel> ReadAsync(string id)
+        {
+            var result = await DataStore.ReadAsync(id);
+            return result; 
+        }
+
     }
 }
