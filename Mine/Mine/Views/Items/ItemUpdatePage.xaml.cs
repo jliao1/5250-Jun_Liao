3a0718b8b98655a168262b2e2dce130d06221b9a
june.liao <june.liao.bashpole@gmail.com>
2022-01-23 19:17:47 -0800

    Added comments to ItemUpdatePage.xaml.cs

--- Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -29,6 +29,10 @@ namespace Mine.Views
             BindingContext = this;
         }
 
+        /// <summary>
+        /// Constructor that takes a ViewModel
+        /// </summary>
+        /// <param name="viewModel"></param>
         public ItemUpdatePage(ItemReadViewModel viewModel)
         {
             InitializeComponent();
