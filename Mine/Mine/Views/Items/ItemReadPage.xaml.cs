1e5ce4f72f47f32e054dbd95a6a23db38ba2e37b
june.liao <june.liao.bashpole@gmail.com>
2022-01-23 16:53:37 -0800

    Add comments in ItemReadPage.xaml.cs

--- Mine/Mine/Views/Items/ItemReadPage.xaml.cs
+++ Mine/Mine/Views/Items/ItemReadPage.xaml.cs
@@ -36,6 +36,11 @@ namespace Mine.Views
             BindingContext = viewModel;
         }
 
+        /// <summary>
+        /// Open the Delete page for this item
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public async void DeleteItem_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(viewModel)));
